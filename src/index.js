df7d4fd4ba3bd638957edfb05bb5e3bec075b5a2
cafrmen <cafrmen@gmail.com>
2023-08-21 23:30:50 -0600

    Homepage created

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const menu = document.getElementById('hamburger');
+const nav = document.getElementById('nav-list');
+const artist = document.getElementById('artist');
+
+function toggleButton() {
+    nav.classList.toggle('show');
+    artist.classList.toggle('hide');
+}
+
+menu.addEventListener('click', toggleButton);
